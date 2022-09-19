8f41f30af3b2edfbec6a279da4cd304b7699e979
davidebruzzo <62437632+davidebruzzo@users.noreply.github.com>
2022-09-19 11:00:43 +0200

    Delete task_manager.py

--- task_manager.py
+++
@@ -1,6 +0,0 @@
-# keypoints:
-#           - takes info of the object's color, position ...
-#           - takes the object's status --> subscribe to a topic "spawner" that is the one 
-#                                           that has object's characteristics
-#           - publish infos on simulation environment (coppeliasim/gazebo) --> msg (??)
-#           - subscribe to the robot's position
