8b95af2f654332a8155d0808fa33cf416501bc9a
davidebruzzo <62437632+davidebruzzo@users.noreply.github.com>
2022-09-19 11:00:35 +0200

    Delete random_spawner.py

--- random_spawner.py
+++
@@ -1,4 +0,0 @@
-# keypoints:
-#           - activated by task manager by a srv
-#           - object spawned when conveyor belt free
-#           - produces an object --> return to task manager status object 
